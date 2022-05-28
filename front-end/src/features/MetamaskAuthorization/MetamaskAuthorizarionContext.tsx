6fda6eb04726f605b501e3e0baac30d96bb5b724
Roberto <robertoasgon@gmail.com>
2022-05-28 18:02:47 -0400

    feat: metamask authorization context

---
+++ front-end/src/features/MetamaskAuthorization/MetamaskAuthorizarionContext.tsx
@@ -0,0 +1,112 @@
+
+import React, { createContext, useEffect, useState } from 'react';
+import { MetaMaskInpageProvider } from '@metamask/providers';
+
+
+declare global {
+  interface Window {
+    ethereum: MetaMaskInpageProvider;
+  }
+}
+
+
+class UserAccount {
+  constructor(partial: Partial<UserAccount>) {
+    Object.assign(this, partial);
+  }
+
+  public address = '';
+  public name = '';
+  public email = '';
+  public profile_picture = '';
+}
+
+
+interface IMetamaskContext {
+  userAccount: UserAccount;
+  isConnectedAccount: boolean;
+}
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+const MetamaskContext = createContext<Partial<IMetamaskContext>>(
+  {}
+);
+
+const MetamaskProvider: React.FC<Props> = ({ children }) => {
+  const [userAccount, setUserAccount] = useState<UserAccount>(new UserAccount({}))
+  const [isConnectedAccount, setIsConnectedAccount] = useState(false)
+
+  console.log('userAccount', userAccount)
+  console.log('isConnectedAccount',isConnectedAccount)
+
+  const doConnectAccount = async () => {
+    try {
+      const provider = window?.ethereum;
+
+      if (!provider) return alert('Please install MetaMask!')
+
+      if (provider !== window.ethereum) {
+        return alert('Do you have multiple wallets installed?')
+      }
+
+      await window.ethereum.request({ method: 'eth_requestAccounts' })
+        .then((accounts) => {
+          const account:string = JSON.parse(JSON.stringify(accounts))[0];
+          if(Boolean(account)) {
+            return setUserAccount(new UserAccount({address: account})) 
+          }
+        })
+
+      setIsConnectedAccount(true);  
+    } catch (error) {
+      console.error('ERROR doConnectAccount:', error)
+    }
+  }
+
+  const checkIfAccountIsAlreadyConnected = async () => {
+    try {
+      if (!window.ethereum) return alert('Please install metamask ')
+
+      const connectedAccount = await window.ethereum.request({ method: 'eth_accounts' })
+        .then((accounts) => {
+          const account:string = JSON.parse(JSON.stringify(accounts))[0];
+          if(Boolean(account)) {
+            return new UserAccount({address: account}) 
+          }
+        });
+
+      if(connectedAccount) {
+        console.log('connectedAccount',connectedAccount)
+        setUserAccount(connectedAccount)
+        setIsConnectedAccount(true);
+      } {
+        await doConnectAccount()
+      }
+
+    } catch (error) {
+      console.error('ERROR checkIfAccountIsAlreadyConnected:', error)
+    }
+  }
+
+  useEffect(() => {
+    checkIfAccountIsAlreadyConnected()
+  }, [])
+
+  return (
+    <MetamaskContext.Provider
+      value={{
+        userAccount,
+        isConnectedAccount
+      }}
+      >
+        {children}
+    </MetamaskContext.Provider>
+  );
+};
+
+export { MetamaskContext };
+export default MetamaskProvider;
+  
---
+++ front-end/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main >
+        <h1 >
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
